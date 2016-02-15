7b9e05b77f5d0649e974c3a4cd0303c5fc4fa2f9
altay <altai_9393@mail.ru>
2016-02-15 15:50:48 +0600

    Created base project structure.

---
+++ app/src/main/java/com/example/altay/remindmebeta/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.altay.remindmebeta;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by altay on 2/15/16.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
