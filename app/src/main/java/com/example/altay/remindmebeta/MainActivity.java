fd542712df4c93aa64212cd1536058fe3b7d5c14
altay <altai_9393@mail.ru>
2016-02-24 21:46:10 +0600

    Created base project structure.

--- app/src/main/java/com/example/altay/remindmebeta/MainActivity.java
+++ app/src/main/java/com/example/altay/remindmebeta/MainActivity.java
@@ -2,18 +2,18 @@ package com.example.altay.remindmebeta;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.Toolbar;
 
-/**
- * Created by altay on 2/15/16.
- */
-public class MainActivity extends Activity {
+
+public class MainActivity extends AppCompatActivity {
 
     private android.support.v7.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
